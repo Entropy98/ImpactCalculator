679d8a57083ef316cc8a9b8723072acd17879e6a
Nigel nderi <Nigel.Nderi@gmail.com>
2018-02-10 07:05:41 -0500

    Set up

---
+++ src/pdfReaderTest.java
@@ -0,0 +1,20 @@
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import java.io.File;
+import java.io.IOException;
+
+public class pdfReaderTest{
+    private static String getText(File pdfFile) throws IOException {
+        PDDocument doc = PDDocument.load(pdfFile);
+        return new PDFTextStripper().getText(doc);
+    }
+    public static void main(String args[]) {
+        try {
+            String text = getText(new File("C:\\Users\\nigel\\OneDrive\\Documents\\151 HW\\HW 1.pdf"));
+            String[] t = text.trim().split("\\s+");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
